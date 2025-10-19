02c69318743b8e8326495d7849c2d93082b0df34
fady emad <fadyemad933@gmail.com>
2025-10-19 03:48:32 +0300

    add new style to btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { ModeToggle } from "@/components/theme/ModeToggle";
-
 export default function Home() {
   return (
-    <>
-      <ModeToggle />
-    </>
+    <div className="relative w-full h-screen">
+      <div className="absolute bottom-4 right-4">
+        <ModeToggle />
+      </div>
+    </div>
   );
 }
