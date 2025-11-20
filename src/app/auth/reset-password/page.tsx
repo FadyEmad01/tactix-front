f3b2e65daddee074105dcc03fa6024ae4ef8b3d0
fady emad <fadyemad933@gmail.com>
2025-11-20 14:49:43 +0200

    Add new theme styles

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -3,7 +3,7 @@ import { ResetPasswordForm } from '@/components/auth/ResetPassword'
 
 export default function page() {
     return (
-        <div className="bg-muted dark:bg-background flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+        <div className="bg-background flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm">
                 <ResetPasswordForm />
             </div>
